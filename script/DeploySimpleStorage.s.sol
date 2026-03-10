6c112a1baefb4fc453c328c3b84f32fe2f98e31b
Emmy <cyubahiroemmy12@gmail.com>
2026-03-10 21:28:16 +0200

    Updated the Scripts

--- script/DeploySimpleStorage.s.sol
+++ script/DeploySimpleStorage.s.sol
@@ -3,6 +3,11 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "../forge-std/Script.sol";
+import {SimpleStorage} from "../src/SimpleStorage.sol";
 
 
-contract DeploySimpleStorage {}
+contract DeploySimpleStorage is Script{
+    function run() external returns(SimpleStorage) {
+        vm.startBroadcast();
+    }
+}
